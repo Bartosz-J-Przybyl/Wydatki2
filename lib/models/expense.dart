11af549acd110c263341b068629c2cd4b1343b99
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2023-08-24 18:57:48 +0200

    change expanse model with adding enum

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
+    required this.category,
     required this.title,
     required this.amount,
     required this.date,
-  }): id = uuid.v4();
+  }) : id = uuid.v4();
 
   final String title;
   final double amount;
   final String id;
   final DateTime date;
+  final Category category;
 }
