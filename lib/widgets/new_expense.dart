67eedd4a5f2e1b23101cecb2cd0b35f9d2eec18e
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2023-08-29 17:31:09 +0200

    add TextEditingController and TextField

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,16 +8,54 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amauntController = TextEditingController();
+
+  // var _enteredTitle = "";
+
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amauntController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(label: Text("Title")),
+            decoration: const InputDecoration(
+              label: Text("Title"),
+            ),
           ),
+          TextField(
+            controller: _amauntController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: "\$ ",
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text("cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("save expense"),
+              )
+            ],
+          )
         ],
       ),
     );
